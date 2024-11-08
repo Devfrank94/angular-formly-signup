ba589a8be4b928d5427f601ce39a21569224607b
YYE7459 <francesco.murro@infocamere.it>
2024-11-08 17:21:03 +0100

    Add dynamic title in all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import {Error404Component} from './pages/error404/error404.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) },
+  { path: 'home',title: "Home", loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'signup', title: "Registrati", loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) },
   { path: "404", component: Error404Component, title: "Errore 404 Pagina non trovata" },
   { path: "**", redirectTo: "/404" },
 ];
