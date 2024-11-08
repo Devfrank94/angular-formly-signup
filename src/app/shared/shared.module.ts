a8725603f0927cef66e7de0ccdee66d3d83488a3
YYE7459 <francesco.murro@infocamere.it>
2024-11-08 12:19:49 +0100

    add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormSignupComponent } from './components/form-signup/form-signup.component';
+
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    NavbarComponent,
+    FooterComponent,
+    FormSignupComponent
+  ],
+  exports: [
+    NavbarComponent,
+    CardComponent,
+    FooterComponent,
+    FormSignupComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  // exports:
+})
+export class SharedModule { }
