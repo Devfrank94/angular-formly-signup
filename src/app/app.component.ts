9f3d5c005281fd6516670ffc4bc7fe9f0f585447
YYE7459 <francesco.murro@infocamere.it>
2024-11-08 17:19:16 +0100

    Add back to top component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 
 @Component({
@@ -6,6 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-formly-signup'
+export class AppComponent implements OnInit {
+  title = 'Angular-formly-signup'
+  mybutton!: any;
+
+  ngOnInit(): void {
+
+  this.mybutton = document.getElementById('myBtn');
+  }
+
+  // When the user scrolls down 20px from the top of the document, show the button
+
+  scrollFunction() {
+    if (this.mybutton && (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)) {
+      this.mybutton.style.display = 'block';
+    } else {
+      this.mybutton.style.display = 'none';
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  topFunction() {
+    // document.body.scrollTop
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // document.documentElement.scrollTop = 0;
+  }
 }
+
