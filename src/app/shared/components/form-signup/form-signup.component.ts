9dbfd33e710f5dc9fa1e0e3e2435c3d2ae2b5486
YYE7459 <francesco.murro@infocamere.it>
2025-01-20 16:00:34 +0100

    add formly-signup

--- src/app/shared/components/form-signup/form-signup.component.ts
+++ src/app/shared/components/form-signup/form-signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-signup',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrl: './form-signup.component.css'
 })
 export class FormSignupComponent {
+
+  form = new FormGroup({});
+  model = { username: '', password: '', email: '' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'username',
+      type: 'input',
+      props: {
+        label: 'Username',
+        required: true,
+      },
+    },
+    {
+      key: 'password',
+      type: 'input',
+      props: {
+        label: 'Password',
+        type: 'password',
+        required: true,
+      },
+    },
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email',
+        type: 'email',
+        required: true,
+      },
+    },
+  ];
+
+  // constructor(private http: HttpClient) {}
+
+  onSubmit(model: any) {
+    console.log(model);
+    // if (this.form.valid) {
+    //   this.http.post('http://localhost:3001/users', this.model)
+    //     .subscribe(response => {
+    //       console.log('User registered successfully!', response);
+    //     });
+    // }
+  }
 
 }
